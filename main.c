225f2ad281b550b6cb440e7500e9cc7db22163d4
omaymaaa <61546431+omaymaaa@users.noreply.github.com>
2020-06-19 11:42:15 +0100

    collision

--- main.c
+++ main.c
@@ -1,81 +1,78 @@
+/**
+* @file main.c
+* @brief Testing Program
+* @author Oumaima
+* @version 0.1
+* @date Jun 1, 2020
+*
+* Testing program for AI2
+*
+*/
 
 
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
-#include "enig.h"
-#include<time.h>
+#include <SDL/SDL_ttf.h>
+#include "AI2.h"
 
 int main()
-{ 
+{
+  SDL_Surface *screen=NULL ;
+  int running=1,collision,i,temp_prec,temp_actu,test,animation=0 ;
+  float D,frame=0 ;
 
-	SDL_Surface *screen;
-	enigme  e;
-	int s,r,run =1,running=1,alea;
-	char image[30]="";
-	SDL_Event event;
-	srand(time(NULL));
+
+  Objet perso,zombie,tab[4] ;
+
+   SDL_Init(SDL_INIT_VIDEO);
+
+    screen = SDL_SetVideoMode(width, height, 32, SDL_HWSURFACE|SDL_DOUBLEBUF);
+    
+    initialiser (&perso,tab)  ;
+    setup (screen,&perso,tab) ;
+    
+    SDL_EnableKeyRepeat(10, 10);
+
+    while(running){
+     deplacement_objet(&perso,&running) ;
+     calculer_centre_rayon (&perso,tab) ;
+     D=calculer_distance (&perso,tab) ;
+     collision=verif_collision (&perso,tab,D ) ;
+     if(collision && animation!=1)
+     {
+      animation=1 ; 
+
+     }
+          else if (animation!=1 && collision!=1)
+     {
+       SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255)) ;
+       SDL_BlitSurface(tab[0].img,NULL,screen,&(tab[0].pos)) ;
+       SDL_BlitSurface(perso.img,NULL,screen,&(perso.pos)) ;
+       SDL_Flip(screen) ;
+     }
+          if(animation)
+     {
+       SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255)) ;
+       annimation_feu(screen ,tab,&frame) ;
+       SDL_BlitSurface(perso.img,NULL,screen,&(perso.pos)) ;
+       SDL_Flip(screen) ;    
+     }
+     
+
+
    
-	 
-	 SDL_Init ( SDL_INIT_VIDEO ) ;
+     
 
 
+    }
+    
 
-	 screen=SDL_SetVideoMode(1024,630,32,SDL_HWSURFACE  |  SDL_DOUBLEBUF );
-	 init_enigme(&e);
-	
-	 while (run)
-	 {
-	    running=1,r=0 ;
-	     SDL_PollEvent(&event);
-           switch(event.type)
-            {
-              case SDL_QUIT:
-                run = 0;
-			  break ;
-            }	
-        	
-      generate_afficher ( screen  , image ,&e,&alea) ;
-	        
 
-      s=solution_e (image );
+    
 
-			do
-			{
-			r=resolution (&running,&run);
-			}
-
-	while((r>3 || r<1) && running!=0) ;
-			
-		{	
-     		 while(running)
-			{
-
-				afficher_resultat (screen,s,r,&e) ;
-			       SDL_WaitEvent(&event);
-                    	 switch(event.type)
-                       {
-					     case SDL_QUIT :
-                              running =0 ;
-						      run=0 ;
-					     break ;
-                         case SDL_KEYDOWN :
-						    
-                             switch( event.key.keysym.sym )
-                                  {
-						case  SDLK_ESCAPE: 
-			                           running= 0 ;
-			                        break ;
-			                      }
-						 break ;
-                       }
-                    } 
-		}	
-   }
-      SDL_FreeSurface(screen);
-      SDL_Quit();
-	return 0;
+    return EXIT_SUCCESS;
 }
+
--- makefile
+++ makefile
@@ -1,9 +1,8 @@
-prog:main.o enigf.o 
-	gcc   main.o enigf.o -o prog -lSDL -lSDL_image
+prog:main.o AI2.o 
+	gcc   main.o AI2.o -o prog -lSDL -lSDL_image -lm
 main.o:main.c
 	gcc -c main.c -g
 
-enigf.o:enigf.c
-	gcc -c enigf.c -g 
-
+AI2.o:AI2.c
+	gcc -c AI2.c -g 
 
